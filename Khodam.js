077aa21b706985a840a921b9ba7e084eea14c8e4
Shikuuso <aguspratmit@gmail.com>
2024-08-11 17:08:58 +0700

    Add files via upload

---
+++ Khodam.js
@@ -0,0 +1,17 @@
+document.getElementById('checkButton').addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    const result = document.getElementById('result');
+
+    if (name) {
+        const khodam = checkKhodam(name);
+        result.innerHTML = 'Khodam Anda adalah: <strong></strong>';
+    } else {
+        result.innerHTML = '<span style="color: red;">Silakan masukkan nama Anda!</span>';
+    }
+});
+
+function checkKhodam(name) {
+    const khodams = ['Monyet Kayang', 'Mangga Busuk','Pisang Busuk','Melon Busuk'];
+    const index = Math.floor(Math.random() * khodams.length);
+    return khodams[index];
+}
